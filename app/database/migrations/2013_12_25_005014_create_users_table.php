6277fb9b0fb16ccdff994c35eeed14642aa67610
Andreas Heiberg <heibergandreas@gmail.com>
2014-01-03 01:45:28 +0100

    added unique constraint to users.email

--- app/database/migrations/2013_12_25_005014_create_users_table.php
+++ app/database/migrations/2013_12_25_005014_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('email')->index();
+			$table->string('email')->unique()->index();
 			$table->string('password');
 			$table->string('first_name');
 			$table->string('last_name');
